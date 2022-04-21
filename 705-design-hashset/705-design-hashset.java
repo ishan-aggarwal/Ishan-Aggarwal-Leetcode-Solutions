04319d0688f910e1f04f81a8a31af778242ad292
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-04-21 11:53:16 +0530

    Time: 31 ms (48.58%), Space: 56.5 MB (60.68%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,22 +1,69 @@
 class MyHashSet {
 
-    int[] nums;
+        Entry[] bucket;
+        int size = 1000;
 
-    public MyHashSet() {
-        nums = new int[1_000_001];
-    }
+        public MyHashSet() {
+            bucket = new Entry[size];
+        }
 
-    public void add(int key) {
-        nums[key] = 1;
-    }
+        public void add(int key) {
+            Entry entry = bucket[key % size];
+            if (entry == null) {
+                bucket[key % size] = new Entry(key, null);
+                return;
+            }
+            Entry preEntry = null;
+            while (entry != null) {
+                if (entry.val == key)
+                    return;
+                preEntry = entry;
+                entry = entry.next;
+            }
+            preEntry.next = new Entry(key, null);
+        }
 
-    public void remove(int key) {
-        nums[key] = 0;
-    }
+        public void remove(int key) {
+            Entry entry = bucket[key % size];
+            if (entry == null)
+                return;
+            Entry preEntry = null;
+            while (entry != null) {
+                if (entry.val == key) {
+                    if (preEntry == null) {
+                        bucket[key % size] = entry.next;
+                    } else {
+                        preEntry.next = entry.next;
+                    }
+                    return;
+                }
+                preEntry = entry;
+                entry = entry.next;
+            }
+        }
 
-    public boolean contains(int key) {
-        return nums[key] != 0;
-    }
+        public boolean contains(int key) {
+            Entry entry = bucket[key % size];
+            if (entry == null) {
+                return false;
+            }
+            while (entry != null) {
+                if (entry.val == key)
+                    return true;
+                entry = entry.next;
+            }
+            return false;
+        }
+
+        class Entry {
+            int val;
+            Entry next;
+
+            Entry(int val, Entry next) {
+                this.val = val;
+                this.next = next;
+            }
+        }
 
 }
 
