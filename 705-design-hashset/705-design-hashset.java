86cc455c09f70ff4ad72933357d77f558fc5c4f3
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-04-21 12:09:20 +0530

    Time: 21 ms (69.66%), Space: 56.4 MB (64.42%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,5 +1,24 @@
 class MyHashSet {
+    
+//     int[] nums;
 
+//     public MyHashSet() {
+//         nums = new int[1_000_001];
+//     }
+
+//     public void add(int key) {
+//         nums[key] = 1;
+//     }
+
+//     public void remove(int key) {
+//         nums[key] = 0;
+//     }
+
+//     public boolean contains(int key) {
+//         return nums[key] != 0;
+//     }
+    
+    
         Entry[] bucket;
         int size = 1000;
 
