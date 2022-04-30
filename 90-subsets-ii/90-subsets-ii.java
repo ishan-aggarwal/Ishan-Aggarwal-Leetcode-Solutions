05f9cb0a0ef43a2216de9d392a01a909794be8bb
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-04-30 16:55:33 +0530

    Time: 16 ms (5.92%), Space: 44 MB (59.14%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        Arrays.sort(nums);
+        solve(0, nums, curr, ans);
+        
+        return ans;
+    }
+    
+    private void solve(int idx, int[] nums, List<Integer> curr, List<List<Integer>> ans) {
+        
+        if (idx == nums.length) {
+            if (!ans.contains(curr)) {
+                ans.add(new ArrayList<>(curr));
+            }
+            return;
+        }
+        
+        curr.add(nums[idx]);
+        solve(idx + 1, nums, curr, ans);
+        
+        curr.remove(curr.size() - 1);
+        solve(idx + 1, nums, curr, ans);        
+    }
+}
