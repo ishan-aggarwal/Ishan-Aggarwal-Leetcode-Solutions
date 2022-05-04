11fc11b6a78a6d7e270e37ea5764c30accc12dbd
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-05-04 07:25:09 +0530

    Time: 64 ms (24.62%), Space: 82.6 MB (33.97%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,23 +1,43 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
+
         
-        int i = 0, j = nums.length - 1;
+        // Approach 1 - sorting
+//         Arrays.sort(nums);
         
-        int countOperations = 0;
+//         int i = 0, j = nums.length - 1;
         
-        while (i < j) {
-            if (nums[i] + nums[j] == k) {
-                countOperations++;
-                i++;
-                j--;
-            } else if (nums[i] + nums[j] > k) {
-                j--; 
+//         int countOperations = 0;
+        
+//         while (i < j) {
+//             if (nums[i] + nums[j] == k) {
+//                 countOperations++;
+//                 i++;
+//                 j--;
+//             } else if (nums[i] + nums[j] > k) {
+//                 j--; 
+//             } else {
+//                 i++;
+//             }
+//         }
+        
+//         return countOperations;
+        
+        
+        Map<Integer,Integer> map = new HashMap<>();
+        int count=0;
+        
+        for(int i=0; i<nums.length; i++){
+            //to check if that k-nums[i] present and had some value left or already paired
+            if (map.containsKey(k-nums[i]) && map.get(k-nums[i]) > 0) {
+                count++;
+                map.put(k-nums[i], map.get(k-nums[i]) - 1);
             } else {
-                i++;
+                //getOrDefault is easy way it directly checks if value is 0 returns 0 where I added 1
+                //and if some value is present then it return that value "similar to map.get(i)" and I added 1 on it 
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
         }
-        
-        return countOperations;
+        return count;        
     }
 }
