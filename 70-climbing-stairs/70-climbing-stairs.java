37bd62a638146fa8500cdc13304747d0f2e8ac3e
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-05-04 09:39:13 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (41.57%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    // TC - O(N) and SC - O(1)
     public int climbStairs(int n) {
         
         // base cases
@@ -7,6 +9,15 @@ class Solution {
         
         int a = 1, b = 1;
         int c = a + b;
+        
+        
+        // Consider the simple scenario here
+        // as we need to add the number of ways to reach to the destination from start or start from destination
+        // if we are standing at Nth (3) stair...
+        // and we take one step down to come to 2nd stair...then we know from 2nd stair there are two ways to reach zeroth stair
+        // similary if we are standing of Nth (3) stair...
+        // and we take two steps down to come to the 1st stair... then we know from there there is only 1 way to reach the zeroth stair..
+        // so we need to combine all possible ways by either jumping one stair at a time or two stairs at a time
         
         for (int i = 2; i <= n; i++) {
             c = a + b;
