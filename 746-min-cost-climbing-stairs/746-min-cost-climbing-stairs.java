9980a7ea0530079f7e411da57c74f104b9ac86fd
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-05-04 15:44:33 +0530

    Time: 1 ms (82.12%), Space: 44 MB (32.98%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+
+
+        int n = cost.length;
+        // minimum cost of first two stairs would be the cost of 
+        // getting to that stair it self, becuase, cost is positive, 
+        // so if for second step, it would be minimal if you jump directly to it.
+        int f = cost[0];
+        int s = cost[1];
+        
+        // edge case when there are two stairs, return the minimum.
+        if(n <= 2) return Math.min(f, s);
+        
+        // calculate the cost of climibing up the stair as
+        
+        int curr;
+        for(int i = 2; i < n; i++) {
+            // toatl_cost(stair n) = cost(stair n) + min(total_cost(stair n - 1), total_cost(stair n - 2))
+            curr = cost[i] + Math.min(f, s);
+            
+            // now for next iteration update the n-2 pointer with n - 1 cost
+            f = s;
+            // n - 1 with n
+            s = curr;
+        }
+        
+        return Math.min(f, s);
+    }
+}
