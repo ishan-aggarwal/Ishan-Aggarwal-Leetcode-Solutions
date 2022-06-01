6f683dd62d239f2def86e76044cf4521a3f42cb8
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-06-01 14:43:48 +0530

    Time: 8 ms (96.25%), Space: 42.4 MB (93.54%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,27 +1,40 @@
 class Solution {
-    int[][] matrix, helper;
-    int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     public int longestIncreasingPath(int[][] matrix) {
-        helper = new int[matrix.length][matrix[0].length];
-        this.matrix= matrix;
-        int path = 0;
-        for(int i=0;i<matrix.length;i++) {
-            for(int j=0;j<matrix[0].length;j++) 
-                path = Math.max(path, dfs(i, j));
-        }
         
-        return path;
-    }
-     
-    private int dfs(int x, int y) {
-        if (helper[x][y] != 0) return helper[x][y];
-        for(int d=0;d<dirs.length;d++) {
-            int i = dirs[d][0] + x, j = dirs[d][1] + y;
-            if(i >= 0 && i < matrix.length && j >= 0 && j < matrix[0].length && matrix[x][y] < matrix[i][j]) {
-                helper[x][y] = Math.max(helper[x][y], dfs(i, j));
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+        
+        int answer = 1;
+        
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                answer = Math.max( answer, dfs(r, c, matrix, -1, dp));
             }
         }
-        return ++helper[x][y];
+        
+        return answer;
     }
-
+    
+    private static int dfs(int rowIdx, int colIdx, int[][] matrix, int prevValue, int[][] dp) {
+        
+        if (rowIdx < 0 || rowIdx >= matrix.length
+                || colIdx < 0 || colIdx >= matrix[0].length
+                || matrix[rowIdx][colIdx] <= prevValue) {
+            return 0;
+        }
+        
+        if (dp[rowIdx][colIdx] != 0) {
+            return dp[rowIdx][colIdx];
+        }
+        
+        int res = 1;
+        res = Math.max(res, 1 + dfs(rowIdx + 1, colIdx, matrix, matrix[rowIdx][colIdx], dp));
+        res = Math.max(res, 1 + dfs(rowIdx - 1, colIdx, matrix, matrix[rowIdx][colIdx], dp));
+        res = Math.max(res, 1 + dfs(rowIdx, colIdx + 1, matrix, matrix[rowIdx][colIdx], dp));
+        res = Math.max(res, 1 + dfs(rowIdx, colIdx - 1, matrix, matrix[rowIdx][colIdx], dp));
+        
+        dp[rowIdx][colIdx] = res;
+        return res;
+    }
 }
