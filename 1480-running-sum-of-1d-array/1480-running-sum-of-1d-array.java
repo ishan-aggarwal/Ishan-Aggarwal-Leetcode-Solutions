aaaf8732e2860f87841fe7b78ae4800931ddfa8b
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-06-01 13:31:41 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (8.45%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        
+        int[] res = new int[nums.length];
+        res[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = nums[i] + res[i-1];
+        }
+        
+        return res;
+    }
+}
