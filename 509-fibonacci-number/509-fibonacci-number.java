e51963eaaac6e1f4e9b2c6e3c25f1c3f71a95187
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-07-06 08:35:40 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (57.51%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,16 +1,12 @@
 class Solution {
     public int fib(int n) {
-
         if (n <= 1) return n;
-        
         int a = 0, b = 1, c = 1;
-        
         for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
             b = c;
         }
-        
         return c;
     }
 }
