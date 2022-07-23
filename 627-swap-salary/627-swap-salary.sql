d18346971e55e8abf58e4dfc9ecdface38d4b009
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-07-24 02:56:48 +0530

    Time: 354 ms (30.94%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,10 +1,16 @@
 # Write your MySQL query statement below
 
+# UPDATE 
+#     Salary
+# SET
+#     sex = 
+#         CASE sex
+#             WHEN 'm' THEN 'f'
+#             ELSE 'm'
+#         END;
+
+
 UPDATE 
-    Salary
-SET
-    sex = 
-        CASE sex
-            WHEN 'm' THEN 'f'
-            ELSE 'm'
-        END;
+    salary 
+SET 
+    sex = IF (sex = "m", "f", "m");
