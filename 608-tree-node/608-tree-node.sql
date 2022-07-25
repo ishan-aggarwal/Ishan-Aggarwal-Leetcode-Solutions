840d5dba3700abcedd28c1569003b8ccb0afb04d
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-07-25 07:47:14 +0530

    Time: 409 ms (76.13%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,39 +1,56 @@
 # Write your MySQL query statement below
 
+# SELECT
+#     id, 'Root' AS Type
+# FROM
+#     tree
+# WHERE
+#     p_id IS NULL
+
+# UNION
+
+# SELECT
+#     id, 'Leaf' AS Type
+# FROM
+#     tree
+# WHERE
+#     id NOT IN (SELECT DISTINCT
+#             p_id
+#         FROM
+#             tree
+#         WHERE
+#             p_id IS NOT NULL)
+#         AND p_id IS NOT NULL
+
+# UNION
+
+# SELECT
+#     id, 'Inner' AS Type
+# FROM
+#     tree
+# WHERE
+#     id IN (SELECT DISTINCT
+#             p_id
+#         FROM
+#             tree
+#         WHERE
+#             p_id IS NOT NULL)
+#         AND p_id IS NOT NULL
+# ORDER BY id;
+
+
+
 SELECT
-    id, 'Root' AS Type
+    id AS `Id`,
+    CASE
+        WHEN tree.id = (SELECT atree.id FROM tree atree WHERE atree.p_id IS NULL)
+          THEN 'Root'
+        WHEN tree.id IN (SELECT atree.p_id FROM tree atree)
+          THEN 'Inner'
+        ELSE 'Leaf'
+    END AS Type
 FROM
     tree
-WHERE
-    p_id IS NULL
+ORDER BY `Id`;
 
-UNION
 
-SELECT
-    id, 'Leaf' AS Type
-FROM
-    tree
-WHERE
-    id NOT IN (SELECT DISTINCT
-            p_id
-        FROM
-            tree
-        WHERE
-            p_id IS NOT NULL)
-        AND p_id IS NOT NULL
-
-UNION
-
-SELECT
-    id, 'Inner' AS Type
-FROM
-    tree
-WHERE
-    id IN (SELECT DISTINCT
-            p_id
-        FROM
-            tree
-        WHERE
-            p_id IS NOT NULL)
-        AND p_id IS NOT NULL
-ORDER BY id;
