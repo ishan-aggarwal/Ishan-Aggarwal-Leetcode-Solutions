a0884d18cff64887ad2ce90bed729c1ad00720b8
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-07-27 07:37:30 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (71.23%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    private TreeNode flattenTree(TreeNode node) {
+        
+        // Handle the null scenario
+        if (node == null) {
+            return null;
+        }
+            
+        // For a leaf node, we simply return the
+        // node as is.
+        if (node.left == null && node.right == null) {
+            return node;
+        }
+        
+        //Recursively flatten the left subtree
+        TreeNode leftTail = this.flattenTree(node.left);
+        
+        // Recursively flatten the right subtree
+        TreeNode rightTail = this.flattenTree(node.right);
+        
+        // If there was a left subtree, we shuffle the connections
+        // around so that there is nothing on the left side
+        // anymore.
+        if (leftTail != null) {
+            leftTail.right = node.right;
+            node.right = node.left;
+            node.left = null;
+        }
+        
+        // We need to return the "rightmost" node after we are
+        // done wiring the new connections. 
+        return rightTail == null ? leftTail : rightTail;
+    }
+    
+    public void flatten(TreeNode root) {
+        
+        this.flattenTree(root);
+    }
+}
