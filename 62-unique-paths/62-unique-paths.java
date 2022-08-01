9215d65c9a502406366da0185bfe42a0f9ab0696
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-08-01 07:29:21 +0530

    Time: 0 ms (100.00%), Space: 39 MB (96.35%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        
+        // base case if there is only 1 row or 1 column
+        if (m == 0 || n == 0) {
+            return 1;
+        }
+        
+        // take a matrix to store the dp states
+        int[][] dpState = new int[m][n];
+        
+        // fill the first col with 1
+        for (int i = 0; i < m; i++) {
+            dpState[i][0] = 1;        
+        }
+        
+        // fill the first row with 1
+        for (int j = 0; j < n; j++) {
+            dpState[0][j] = 1;
+        }
+        
+        
+        // iterate over the grid
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                
+                // dp expression
+                // where dp[i][j] represents the unique ways to reach i,j using i-1,j or i,j-1
+                dpState[i][j] = dpState[i - 1][j] + dpState[i][j - 1];
+            }
+        }
+        
+        // return ans
+        return dpState[m - 1][n - 1];
+    }
+}
