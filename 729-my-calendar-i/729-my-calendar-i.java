1148d08b1a679fbe7294b0b7ee15da8f9f585b7f
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-08-03 07:40:52 +0530

    Time: 29 ms (78.92%), Space: 55.4 MB (9.46%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,16 +1,38 @@
-public class MyCalendar {
-    List<int[]> calendar;
+// Brute Force
+// public class MyCalendar {
+//     List<int[]> calendar;
+
+//     MyCalendar() {
+//         calendar = new ArrayList();
+//     }
+
+//     public boolean book(int start, int end) {
+//         for (int[] iv: calendar) {
+//             if (iv[0] < end && start < iv[1]) return false;
+//         }
+//         calendar.add(new int[]{start, end});
+//         return true;
+//     }
+// }
+
+
+// Balanced Tree
+class MyCalendar {
+    TreeMap<Integer, Integer> calendar;
 
     MyCalendar() {
-        calendar = new ArrayList();
+        calendar = new TreeMap();
     }
 
     public boolean book(int start, int end) {
-        for (int[] iv: calendar) {
-            if (iv[0] < end && start < iv[1]) return false;
+        Integer prev = calendar.floorKey(start),
+                next = calendar.ceilingKey(start);
+        if ((prev == null || calendar.get(prev) <= start) &&
+                (next == null || end <= next)) {
+            calendar.put(start, end);
+            return true;
         }
-        calendar.add(new int[]{start, end});
-        return true;
+        return false;
     }
 }
 
