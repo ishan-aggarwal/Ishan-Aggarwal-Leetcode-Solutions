8499d4ab7d7afcfcfc4ca3ff96e0709d7305d327
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-08-05 07:52:46 +0530

    Time: 9 ms (69.03%), Space: 43.5 MB (40.06%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,57 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character,Integer> map=new HashMap<>();
-            int i=-1;
-            int j=-1;
-            int ans=0;
-            while(true){
-            boolean f1=false;
-            boolean f2=false;
-            while(i<s.length()-1){
-                //acquire
-               i++;
-               f1=true;
-               char ch=s.charAt(i);
-               map.put(ch,map.getOrDefault(ch,0)+1);
-               int count=0;
-               for(char key:map.keySet()){
-                   count=Math.max(count,map.get(key));
-               }
-               int replace=(i-j) - count;
-               if(replace<=k) {
-                   ans=Math.max(i-j,ans);
-               } 
-               else break;
-                   
-             }
-                
-             while(j<i){
-               //release   
-               f2=true;  
-               j++;
-               char ch=s.charAt(j);
-               isremove(map,ch);
-               int count=0;
-               for(char key:map.keySet()){
-                   count=Math.max(count,map.get(key));
-               }
-               int replace=(i-j) - count;  
-               if(replace<=k){
-                   ans=Math.max(i-j,ans);
-                   break;
-               }
-               else continue;  
-             }   
-             if(f1==false && f2==false) break;   
-                
-         }
         
+       //base case
+        if (s.length() == k || k - 1 == s.length()) {
+            return s.length();
+        }
+
+        int left=0, ans=0, maxFreqChar = 0;
+        int[] fa = new int[26];
+
+        for (int right = 0; right < s.length(); right++) {
+            fa[s.charAt(right) - 'A']++;
+            maxFreqChar = Math.max(maxFreqChar, fa[s.charAt(right) - 'A']);
+            //length of window - total frequency of highest occuring character = k
+            if ((right - left + 1) - maxFreqChar > k) {
+                fa[s.charAt(left) - 'A']--;
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
         return ans;
-    }
-    
-    
-    static void isremove(HashMap<Character,Integer> map,char ch){
-        if(map.get(ch)==1) map.remove(ch);
-        else map.put(ch,map.get(ch)-1);
     }
 }
