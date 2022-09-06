659f87ff80b60ce5fcc366320cc70f599df87049
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-09-06 07:52:54 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (65.32%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -13,27 +13,39 @@
  *     }
  * }
  */
+
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-        return containsOne(root) ? root : null;
-    }
-
-    public boolean containsOne(TreeNode node) {
-        if (node == null) return false;
-        
-        // Check if any node in the left subtree contains a 1.
-        boolean leftContainsOne = containsOne(node.left);
-        
-        // Check if any node in the right subtree contains a 1.
-        boolean rightContainsOne = containsOne(node.right);
-
-        // If the left subtree does not contain a 1, prune the subtree.
-        if (!leftContainsOne) node.left = null;
-        
-        // If the right subtree does not contain a 1, prune the subtree.
-        if (!rightContainsOne) node.right = null;
-        
-        // Return true if the current node, its left or right subtree contains a 1.
-        return node.val == 1 || leftContainsOne || rightContainsOne;
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.val == 0 && root.left == null && root.right == null) root = null;
+        return root;
     }
 }
+
+
+// class Solution {
+//     public TreeNode pruneTree(TreeNode root) {
+//         return containsOne(root) ? root : null;
+//     }
+
+//     public boolean containsOne(TreeNode node) {
+//         if (node == null) return false;
+        
+//         // Check if any node in the left subtree contains a 1.
+//         boolean leftContainsOne = containsOne(node.left);
+        
+//         // Check if any node in the right subtree contains a 1.
+//         boolean rightContainsOne = containsOne(node.right);
+
+//         // If the left subtree does not contain a 1, prune the subtree.
+//         if (!leftContainsOne) node.left = null;
+        
+//         // If the right subtree does not contain a 1, prune the subtree.
+//         if (!rightContainsOne) node.right = null;
+        
+//         // Return true if the current node, its left or right subtree contains a 1.
+//         return node.val == 1 || leftContainsOne || rightContainsOne;
+//     }
+// }
