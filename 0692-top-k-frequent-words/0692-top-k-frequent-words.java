af9d75da8c7db0d963b6e062984198fc15bc8f42
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2022-10-19 09:11:04 +0530

    Time: 15 ms (25.78%), Space: 44.9 MB (72.83%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,30 +1,23 @@
 class Solution {
-    public List < String > topKFrequent(String[] words, int k) {
-        Map<String, Integer > map = new HashMap <> ();
-        List < String > ans = new LinkedList < > ();
+    
+    // Steps:
+    // 1. Count the frequency of each word and store the records by a HashMap cnt. O(N)
+    // 2. Define the sorting order as what the problem indicates (by frequencies from high to low, and then from lexicographically smaller ones     // to larger ones when we meet a tie in frequency) and sort the entire list of all words. O( N log N)
+    // 3. Only return the first k words as the returned result.
 
-        for (int i = 0; i < words.length; i++) {
-
-            if (map.containsKey(words[i]))
-                map.put(words[i], map.get(words[i]) + 1);
-            else
-                map.put(words[i], 1);
+    /*
+        Time Complexity: O(Nlog⁡N). We count the frequency of each word in O(N) time, and then we sort the given words in O(NlogN) time.
+        Space Complexity: O(N), the space used to store frequencies in a HashMap and return a slice from a sorted list of length O(N).
+    */
+    
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
         }
-
-        PriorityQueue<String> pq = new PriorityQueue <> ((a, b) -> map.get(a) == map.get(b) ? b.compareTo(a) : map.get(a) - map.get(b)); //-1 means no swap
-        //System.out.println(pq);
-
-        for (String s : map.keySet()) {
-            pq.offer(s);
-            System.out.println(pq);
-            if (pq.size() > k) {
-                pq.poll();
-                System.out.println(pq);
-            }
-        }
-        while (!pq.isEmpty())
-            ans.add(0, pq.poll());
-
-        return ans;
+        List<String> candidates = new ArrayList<>(cnt.keySet());
+        Collections.sort(candidates,
+                (w1, w2) -> cnt.get(w1).equals(cnt.get(w2)) ? w1.compareTo(w2) : cnt.get(w2) - cnt.get(w1));
+        return candidates.subList(0, k);
     }
 }
