f02fd83647de3158ae5973be3b91d28b4984970b
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-01-03 01:42:56 +0530

    Time: 6 ms (91.59%) | Memory: 42.6 MB (40.98%) - LeetSync

--- 1316-fizz-buzz-multithreaded/fizz-buzz-multithreaded.java
+++ 1316-fizz-buzz-multithreaded/fizz-buzz-multithreaded.java
@@ -1,57 +1,75 @@
 import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
 
 class FizzBuzz {
     private int n;
-    Semaphore A,B,C,D;
+    private Semaphore number = new Semaphore(1);
+    private Semaphore fizz = new Semaphore(0);
+    private Semaphore buzz = new Semaphore(0);
+    private Semaphore fizzbuzz = new Semaphore(0);
+    private int current = 1;
+
     public FizzBuzz(int n) {
         this.n = n;
-        A = new Semaphore(1);
-        B = new Semaphore(0);
-        C = new Semaphore(0);
-        D = new Semaphore(0);
     }
 
-    // printFizz.run() outputs "fizz".
-    public void fizz(Runnable printFizz) throws InterruptedException {
-        for(int i =1;i<=n;i++){
-            A.acquire();
-            if(i%3==0 && i%5 != 0){
-                printFizz.run();
-            }
-            B.release();
-        }
-    }
-
-    // printBuzz.run() outputs "buzz".
-    public void buzz(Runnable printBuzz) throws InterruptedException {
-        for(int i =1;i<=n;i++){
-            B.acquire();
-            if(i%3 != 0 && i%5 == 0){
-                printBuzz.run();
-            }
-            C.release();
-        }
-    }
-
-    // printFizzBuzz.run() outputs "fizzbuzz".
-    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
-        for(int i =1;i<=n;i++){
-            C.acquire();
-            if(i%3==0 && i%5 == 0){
-                printFizzBuzz.run();
-            }
-            D.release();
-        }
-    }
-
-    // printNumber.accept(x) outputs "x", where x is an integer.
     public void number(IntConsumer printNumber) throws InterruptedException {
-        for(int i =1;i<=n;i++){
-            D.acquire();
-            if(i%3 !=0 && i%5 != 0){
-                printNumber.accept(i);
+        while (current <= n) {
+            number.acquire();
+            synchronized (this) {
+                if (current > n) break;
+                if (current % 3 == 0 && current % 5 == 0) {
+                    fizzbuzz.release();
+                } else if (current % 3 == 0) {
+                    fizz.release();
+                } else if (current % 5 == 0) {
+                    buzz.release();
+                } else {
+                    printNumber.accept(current);
+                    current++;
+                    number.release();
+                }
             }
-            A.release();
+        }
+        // Release other semaphores in case they're waiting after `n` is exceeded
+        fizz.release();
+        buzz.release();
+        fizzbuzz.release();
+    }
+
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        while (true) {
+            fizz.acquire();
+            synchronized (this) {
+                if (current > n) break;
+                printFizz.run();
+                current++;
+                number.release();
+            }
+        }
+    }
+
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        while (true) {
+            buzz.acquire();
+            synchronized (this) {
+                if (current > n) break;
+                printBuzz.run();
+                current++;
+                number.release();
+            }
+        }
+    }
+
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (true) {
+            fizzbuzz.acquire();
+            synchronized (this) {
+                if (current > n) break;
+                printFizzBuzz.run();
+                current++;
+                number.release();
+            }
         }
     }
 }
