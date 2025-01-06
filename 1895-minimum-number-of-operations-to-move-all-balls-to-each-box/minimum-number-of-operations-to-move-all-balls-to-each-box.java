7e6640fed40bc5b58d7b19c565b31d343753dd58
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-01-06 15:53:29 +0530

    Time: 3 ms (94.79%) | Memory: 44.2 MB (98.44%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,22 +1,52 @@
 class Solution {
     public int[] minOperations(String boxes) {
         
+        // Appraoch:1 Optimised Approach
         int n = boxes.length();
-        int[] result = new int[n];
-        Set<Integer> ballsPresent = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            int val = boxes.charAt(i) - '0'; // 0  or 1
-            if (val == 1) {
-                ballsPresent.add(i);
-            }
+        int[] distances = new int[n];
+
+        int[] leftSum = new int[n];
+        int[] rightSum = new int[n];
+        int count = boxes.charAt(0) == '1' ? 1 : 0;
+
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = leftSum[i - 1] + count;
+            count += boxes.charAt(i) == '1' ? 1 : 0;
         }
+        //System.out.println(Arrays.toString(leftSum));
+
+        count = boxes.charAt(n -1) == '1' ? 1 : 0;
+        
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] = rightSum[i + 1] + count;
+            count += boxes.charAt(i) == '1' ? 1 : 0;
+        }
+        //System.out.println(Arrays.toString(rightSum));
 
         for (int i = 0; i < n; i++) {
-            for (int index : ballsPresent) {
-                result[i] += Math.abs(i - index);
-            }
+            distances[i] = leftSum[i] + rightSum[i];
         }
         
-        return result;
+        return distances;
+
+
+        // Appraoch:2 done by the brute force way
+        // int n = boxes.length();
+        // int[] result = new int[n];
+        // Set<Integer> ballsPresent = new HashSet<>();
+        // for (int i = 0; i < n; i++) {
+        //     int val = boxes.charAt(i) - '0'; // 0  or 1
+        //     if (val == 1) {
+        //         ballsPresent.add(i);
+        //     }
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int index : ballsPresent) {
+        //         result[i] += Math.abs(i - index);
+        //     }
+        // }
+        
+        // return result;
     }
 }
