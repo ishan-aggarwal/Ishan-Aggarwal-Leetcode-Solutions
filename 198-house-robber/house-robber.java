fe59c7fd214df0d9066e44873da33783955a6508
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-03-08 19:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (33.88%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,24 +1,22 @@
 class Solution {
-
-    int[] dp;
-
     public int rob(int[] nums) {
+
         int n = nums.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        solve(nums, n - 1);
+
+        if (n <= 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int take = nums[i] + dp[i - 2];
+            int dont = dp[i - 1];
+            dp[i] = Math.max(take, dont);
+        }
 
         return dp[n - 1];
-    }
-
-    public int solve(int[] nums, int idx) {
-        if (idx < 0) {
-            return 0;
-        }
-        if (dp[idx] != -1) return dp[idx];
-        int dont = solve(nums, idx - 1);
-        int take = nums[idx] + solve(nums, idx - 2);
-        dp[idx] = Math.max(dont, take);
-        return dp[idx];
     }
 }
