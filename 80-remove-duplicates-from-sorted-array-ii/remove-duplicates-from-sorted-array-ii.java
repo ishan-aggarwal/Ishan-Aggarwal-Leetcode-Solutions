6b4f1426b7c875f3db285989fc1438c1cf8fcdef
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-05-16 23:23:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (44.68%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length < 2) {
+        if (nums.length <= 2) {
             return nums.length;
         }
         int i = 2;
