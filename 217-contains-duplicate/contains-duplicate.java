9251512d341c22a585617267d70567c3802666c2
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-23 13:05:33 +0530

    Time: 13 ms (89.03%) | Memory: 58.4 MB (45.75%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,8 +2,11 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int element : nums) {
+            if (set.contains(element)) {
+                return true;
+            }
             set.add(element);
         }
-        return set.size() != nums.length;
+        return false;
     }
 }
