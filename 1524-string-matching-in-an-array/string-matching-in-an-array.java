ee98942ac71c3e30a151952ed88d081241ab389e
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-24 20:39:17 +0530

    Time: 5 ms (36.63%) | Memory: 42.2 MB (71.08%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,57 +1,16 @@
 class Solution {
-
-    class TrieNode {
-
-        // Tracks how many times this substring appears in the Trie.
-        int frequency;
-        // Maps characters to their respective child nodes.
-        Map<Character, TrieNode> childNodes;
-
-        TrieNode() {
-            frequency = 0;
-            childNodes = new HashMap<>();
-        }
-    }
-
     public List<String> stringMatching(String[] words) {
-        List<String> matchingWords = new ArrayList<>();
-        TrieNode root = new TrieNode(); // Initialize the root of the Trie.
-
-        // Insert all suffixes of each word into the Trie.
-        for (String word : words) {
-            for (int startIndex = 0; startIndex < word.length(); startIndex++) {
-                // Insert each suffix starting from index startIndex.
-                insertWord(root, word.substring(startIndex));
+        Set<String> set = new HashSet<>();
+        int len = words.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i != j) {
+                    if (words[j].contains(words[i])) {
+                        set.add(words[i]);
+                    }
+                }
             }
         }
-
-        // Check each word to see if it exists as a substring in the Trie.
-        for (String word : words) {
-            if (isSubstring(root, word)) {
-                matchingWords.add(word);
-            }
-        }
-
-        return matchingWords;
-    }
-
-    private void insertWord(TrieNode root, String word) {
-        TrieNode currentNode = root;
-        for (char c : word.toCharArray()) {
-            // If the character already exists as a child node, move to it.
-            currentNode.childNodes.putIfAbsent(c, new TrieNode());
-            currentNode = currentNode.childNodes.get(c);
-            currentNode.frequency++; // Increment the frequency of the node.
-        }
-    }
-
-    private boolean isSubstring(TrieNode root, String word) {
-        TrieNode currentNode = root;
-        for (char c : word.toCharArray()) {
-            // Traverse the Trie following the characters of the word.
-            currentNode = currentNode.childNodes.get(c);
-        }
-        // A word is a substring if its frequency in the Trie is greater than 1.
-        return currentNode.frequency > 1;
+        return new ArrayList<>(set);
     }
 }
