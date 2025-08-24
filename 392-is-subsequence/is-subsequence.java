a66414151eb8456479acabbcb403188ad64980ba
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-24 18:41:41 +0530

    Time: 1 ms (93.79%) | Memory: 41.3 MB (95.10%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int sPointer = 0, tPointer = 0;
-
-        // iterate through s and t using desired pointers
-        while (sPointer < s.length() && tPointer < t.length()) {
-
-            // now match the character of s and t.
-            // If they are equal, move the sPointer forward.
-            if (s.charAt(sPointer) == t.charAt(tPointer)) {
-                sPointer++;
+        int n = s.length();
+        int m = t.length();
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
             }
-
-            // tPointer should always be moved forward
-            tPointer++;
         }
-
-        // Return true if sPointer has reached the end of s,
-        return sPointer == s.length();
+        return (i == n) ? true : false;
     }
 }
