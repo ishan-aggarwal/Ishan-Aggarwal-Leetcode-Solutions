61649e62bb42aca7078b63fe93791df4a2f77aa0
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-24 18:55:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (65.59%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -12,7 +12,6 @@ class Solution {
 class Solution {
     public int lengthOfLastWord(String s) {
         s = s.trim();
-        
         int length = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) == ' ') {
