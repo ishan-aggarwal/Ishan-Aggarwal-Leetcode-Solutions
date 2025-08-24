55d09e0ef08b8b57406b14fb56c3ef1491485b90
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-24 20:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 49.5 MB (27.89%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
