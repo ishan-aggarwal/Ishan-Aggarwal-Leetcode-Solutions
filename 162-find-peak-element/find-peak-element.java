8df16194211c806da1e39e7dc514de236e299436
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-26 17:53:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (42.69%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,34 +1,25 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int length = nums.length;
-
-        if (length == 1) {
-            return 0;
-        } else if (length == 2) {
-            return nums[0] > nums[1] ? 0 : 1;
-        }
-
-        int left = 0;
-        int right = length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] > safeGet(nums, mid - 1) && nums[mid] > safeGet(nums, mid + 1)) {
+        int l = 0, r = nums.length - 1;
+        int ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= safeGet(nums, mid - 1) && nums[mid] >= safeGet(nums, mid + 1)) {
                 return mid;
-            } else if (nums[mid] > safeGet(nums, mid - 1) && nums[mid] < safeGet(nums, mid + 1)) {
-                left = mid + 1;
+            }
+            if (nums[mid] >= safeGet(nums, mid - 1) && 
+                        nums[mid] < safeGet(nums, mid + 1)) {
+                l = mid + 1;
             } else {
-                right = mid - 1;
+                r = mid - 1;
             }
         }
-
         return -1;
     }
 
-    public int safeGet(int[] nums, int index) {
-        if (index >= 0 && index < nums.length) {
-            return nums[index];
+    public int safeGet(int[] nums, int pos) {
+        if (pos >= 0 && pos < nums.length) {
+            return nums[pos];
         }
         return Integer.MIN_VALUE;
     }
