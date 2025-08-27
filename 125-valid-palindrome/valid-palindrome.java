00e472a9926b1e2184730191592614df448b6bac
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-28 00:23:54 +0530

    Time: 2 ms (98.81%) | Memory: 43.2 MB (69.50%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,32 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
-        int left = 0, right = s.length() - 1;
-
-        while (left <= right) {
-            char leftValue = s.charAt(left);
-            char rightValue = s.charAt(right);
-            if (isAlphanumeric(leftValue) && isAlphanumeric(rightValue)) {
-                
-                if (leftValue != rightValue) {
+        int i = 0, j = s.length() - 1;
+        while (i <= j) {
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+            if (isAlphanumeric(left) && isAlphanumeric(right)) {
+                if (left != right) {
                     return false;
                 }
-
-                left++;
-                right--;
-
-            } else if (isAlphanumeric(leftValue)) {
-                right--;
+                i++;
+                j--;
+            } else if (isAlphanumeric(left)) {
+                j--;
             } else {
-                left++;
+                i++;
             }
         }
-
         return true;
     }
 
-    public boolean isAlphanumeric(char value) {
-        if ((value >= 'a' && value <= 'z') || (value >= '0' && value <= '9')) {
+    private boolean isAlphanumeric(char c) {
+        if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
             return true;
         }
         return false;
