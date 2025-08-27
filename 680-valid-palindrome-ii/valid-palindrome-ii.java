82fddf7838d96e856a402668fdb60aa5478f9383
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-28 00:35:53 +0530

    Time: 4 ms (99.61%) | Memory: 45.7 MB (25.02%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                // Skip either left or right character
+                return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
