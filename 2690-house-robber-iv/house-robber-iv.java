c4b678edf3e69a76fcd5044cd5989afeaf737b55
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-27 22:43:16 +0530

    Time: 19 ms (70.44%) | Memory: 56.5 MB (46.06%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.java
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -8,28 +8,28 @@ class Solution {
             max = Math.max(max, num);
         }
 
-        int l = min;
-        int r = max;
+        int l = min; // The smallest possible capability is the minimum value in nums
+        int r = max; // The largest possible capability is the maximum value in nums
         int ans = 0;
 
         while (l <= r) {
             int mid = l + (r - l) / 2;
+            // Determine if it's possible to rob at least k houses with a maximum capability of mid
             int houses = countHouses(nums, mid);
             if (houses >= k) {
                 ans = mid;
-                r = mid - 1; // search left for smaller capability
+                r = mid - 1; // search left for smaller capability (Q says:: minimum capability of the robber)
             } else {
-                l = mid + 1;
+                l = mid + 1; // try with large capability to atleast rob k houses
             }
         }
-
         return ans;
     }
 
     private int countHouses(int[] nums, int capability) {
         int count = 0;
         for (int i = 0; i < nums.length; ) {
-            if (nums[i] <= capability) { // careful: capability is the max he can steal
+            if (nums[i] <= capability) { // robber can rob if capability is the max
                 count++;
                 i += 2;
             } else {
