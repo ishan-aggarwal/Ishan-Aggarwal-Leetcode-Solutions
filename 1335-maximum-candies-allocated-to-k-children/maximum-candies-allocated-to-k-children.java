39c411acb985adad15280f0950a1736c53648d04
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-08-27 12:11:49 +0530

    Time: 29 ms (44.95%) | Memory: 63 MB (79.00%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,36 +1,41 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
+        // Find the maximum pile size → upper bound of our search space
         int max = Integer.MIN_VALUE;
         for (int c : candies) {
             max = Math.max(c, max);
         }
 
-        // search space 
-        // because I can distribute minimum 1 candy to each children
-        // and in order to ensure maximize the candies - that we are setting to the max of all
-        // if it is feasible, otherwise our binary search will ensure it does not allow it
+        // Binary search over "candies per child"
+        // l = minimum possible candies per child (1)
+        // r = maximum possible candies per child (max pile size)
         long l = 1, r = max;
-        long ans = 0;
+        long ans = 0; // best feasible allocation found so far
+
         while (l <= r) {
-            long mid =  l + (r - l) / 2; // trial candies per child
+            long mid = l + (r - l) / 2; // trial allocation (candies per child)
             long children = countChildren(candies, mid);
-            // means mid candies I am able to give to more than k children
-            // feasible → try more candies
+
             if (children >= k) {
+                // Feasible: we can give at least k children "mid" candies each
+                // Try to increase allocation
                 ans = mid;
-                l = mid + 1; // I should try to increase the number of candies
+                l = mid + 1;
             } else {
-                // not feasible → try less candies distribution 
-                r = mid - 1; // it is not possible to distribute those many candies so I should try to reduce
+                // Not feasible: too few children can be served with "mid" candies
+                // Reduce allocation
+                r = mid - 1;
             }
         }
         return (int) ans;
     }
 
+    // Count how many children can be served if each gets "candiesPerChild" candies
     private long countChildren(int[] candies, long candiesPerChild) {
         long children = 0;
         for (int c : candies) {
-            children += c / candiesPerChild; // how many children can this pile serve
+            // From this pile, we can serve (c / candiesPerChild) children
+            children += c / candiesPerChild;
         }
         return children;
     }
