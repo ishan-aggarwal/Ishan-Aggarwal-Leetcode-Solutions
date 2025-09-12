7453bfc5ebede9e24f049d682f04602c45f77419
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-09-13 00:00:34 +0530

    Time: 3 ms (96.06%) | Memory: 45.9 MB (16.80%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+                return true;
+        }
+        return false;
+    }
+}
