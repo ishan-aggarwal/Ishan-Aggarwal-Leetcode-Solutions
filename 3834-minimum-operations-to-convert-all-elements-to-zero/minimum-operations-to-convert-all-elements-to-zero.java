769ccdfb5ca4733f4bf7fb71caa7e58194487040
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-11-10 11:01:27 +0530

    Time: 140 ms (38.73%) | Memory: 123.3 MB (17.61%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -1,14 +1,16 @@
 class Solution {
 
     public int minOperations(int[] nums) {
-        List<Integer> s = new ArrayList<>();
+        // List<Integer> s = new ArrayList<>();
+        Stack<Integer> s = new Stack<>();
         int res = 0;
         for (int a : nums) {
-            while (!s.isEmpty() && s.get(s.size() - 1) > a) {
-                s.remove(s.size() - 1);
+            while (!s.isEmpty() && s.peek() > a) {
+                // s.remove(s.size() - 1);
+                s.pop();
             }
             if (a == 0) continue;
-            if (s.isEmpty() || s.get(s.size() - 1) < a) {
+            if (s.isEmpty() || s.peek() < a) {
                 res++;
                 s.add(a);
             }
